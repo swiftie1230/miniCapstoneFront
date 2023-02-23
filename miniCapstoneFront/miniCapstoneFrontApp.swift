d63512dd17c2f477ad17786f3b355f3c2da86fe2
swiftie1230 <swiftie1230@naver.com>
2023-02-23 23:42:20 +0900

    Initial Commit

---
+++ miniCapstoneFront/miniCapstoneFrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniCapstoneFrontApp.swift
+//  miniCapstoneFront
+//
+//  Created by 황서진 on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct miniCapstoneFrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
